d5ee77e3712fa4e2e596c940bf1f13b34d470783
Richmond Watkins <richmondwatkins@mac.com>
2014-12-29 12:18:33 -0600

    stop parent from scrolling

--- popup.js
+++ popup.js
@@ -39,30 +39,35 @@ function setUpHoverEvents () {
   links.forEach(function(l,i){
     $(l).unbind();
     var jL = $(l);
-      $(jL).hoverIntent(function(e) {
+      $(jL).hoverIntent({ 
+
+        over: function(e) {
           currentPost = jL.parent().parent().parent().parent();
           currentPost = $(currentPost);
           currenPostID = currentPost.data('fullname');
 
-          console.log(currentPost);
-          currentPost.mouseleave(function(){
-             removePopUpFromView();
-          });
-        
-          if ($('#pop-up').length <= 0) {
-              retrieveComments(l.href, jL); 
-          }else{
-            removePopUpFromView();
-          }
-      }, function(m) {
-        //mouse leave
-      });
+             if ($('#pop-up').length <= 0) {
+                  retrieveComments(l.href, jL); 
+              }else if($('#pop-up').length > 0){
+                removePopUpFromView();
+                retrieveComments(l.href, jL); 
+              }
+      }, 
+      out: function(){
+        console.log('out');
+      },
+      interval: 200,
+      sensitivity: 2
+    });
   });
 }
 
 function setUpPop (jL){
 
-  var popUp =  $('<div id="pop-up"></div>');
+  var popUp =  $('<div id="pop-up" class="trapScroll"></div>');
+  popUp.css('position', '');
+  popUp.css('top', '');
+  popUp.css('right', '');
 
   jL.parent().append(popUp);
 
@@ -83,9 +88,9 @@ function setUpPop (jL){
       popUp.append(loadingIMG);
    }
 
-  $('#pop-up').mouseleave(function() {
-      removePopUpFromView();
-  });
+  // $('#pop-up').mouseleave(function() {
+  //     removePopUpFromView();
+  // });
 }
 
 function retrieveComments (url, jL){
@@ -102,8 +107,11 @@ function retrieveComments (url, jL){
 
           var popUp = $('#pop-up');
 
-          popUp.css('width', $(window).width() /2);
-          popUp.css('max-height', $(window).height() /2);
+          popUp.css('width', $(window).width() / 3);
+          popUp.css('max-height', $(window).height());
+          popUp.css('position', 'fixed');
+          popUp.css('top', '0px');
+          popUp.css('right', '0px');
 
           //settings button
           // var settingsURL = chrome.extension.getURL("settings.png");
@@ -114,7 +122,7 @@ function retrieveComments (url, jL){
 
           if ($('.exit-button').length <= 0) {
             var exitButton = $('<a class="exit-button" href="#"">X</a>');
-            $('#pop-up').append(exitButton);
+            popUp.append(exitButton);
 
             exitButton.click(function(e){
               removePopUpFromView();
@@ -281,6 +289,7 @@ function createComment(c, isChild){
   }else{
     commentDiv.append($('<a id="author" href="/u/'+c.author+'">' + c.author +'</a><span class="votes">'+c.votes+points+'</span>'));
   }
+  
   commentDiv.append($('<div class="comment-text">'+convertedMarkdown+'</div>')); 
 
   return commentDiv;
@@ -359,12 +368,16 @@ function removePopUpFromView (){
 }
 
 function animateClosing(){
-  $( "#pop-up" ).animate({
-      opacity: 0,
-      height: 0
-    }, 100, function() {
-      $('#pop-up').remove();
-  });
+  // $( "#pop-up" ).animate({
+  //     opacity: 0,
+  //     height: 0
+  //   }, 1, function() {
+      var popUp = $('#pop-up');
+      popUp.css('position', '');
+      popUp.css('top', '');
+      popUp.css('right', '');
+      popUp.remove();
+  // });
 }
 
 function setUpSettingsDropDown(settings){
@@ -437,5 +450,85 @@ function checkDocumentHeight(callback){
 function setUpURLS(){
     setUpHoverEvents();
 }
+
+
+
+//======================== stops scroll in comment div
+
+var trapScroll;
+
+(function($){  
+  
+  trapScroll = function(opt){
+    
+    var trapElement;
+    var scrollableDist;
+    var trapClassName = 'trapScroll-enabled';
+    var trapSelector = '.trapScroll';
+    
+    var trapWheel = function(e){
+      
+      if (!$('body').hasClass(trapClassName)) {
+        
+        return;
+        
+      } else {  
+        
+        var curScrollPos = trapElement.scrollTop();
+        var wheelEvent = e.originalEvent;
+        var dY = wheelEvent.deltaY;
+
+        // only trap events once we've scrolled to the end
+        // or beginning
+        if ((dY>0 && curScrollPos >= scrollableDist) ||
+            (dY<0 && curScrollPos <= 0)) {
+
+          opt.onScrollEnd();
+          return false;
+          
+        }
+        
+      }
+      
+    }
+    
+    $(document)
+      .on('wheel', trapWheel)
+      .on('mouseleave', trapSelector, function(){
+        
+        $('body').removeClass(trapClassName);
+      
+      })
+      .on('mouseenter', trapSelector, function(){   
+      
+        trapElement = $(this);
+        var containerHeight = trapElement.outerHeight();
+        var contentHeight = trapElement[0].scrollHeight; // height of scrollable content
+        scrollableDist = contentHeight - containerHeight;
+        
+        if (contentHeight>containerHeight)
+          $('body').addClass(trapClassName); 
+      
+      });       
+  } 
+  
+})($);
+
+var preventedCount = 0;
+var showEventPreventedMsg = function(){  
+  $('#mousewheel-prevented').stop().animate({opacity: 1}, 'fast');
+}
+var hideEventPreventedMsg = function(){
+  $('#mousewheel-prevented').stop().animate({opacity: 0}, 'fast');
+}
+var addPreventedCount = function(){
+  $('#prevented-count').html('prevented <small>x</small>' + preventedCount++);
+}
+
+trapScroll({ onScrollEnd: addPreventedCount });
+$('.trapScroll')
+  .on('mouseenter', showEventPreventedMsg)
+  .on('mouseleave', hideEventPreventedMsg);      
+$('[id*="parent"]').scrollTop(100);
 
 })();
